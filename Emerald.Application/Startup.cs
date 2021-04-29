1b194fded6c570cefd911d49be9123f1843f9f2b
Philipp Hinz <phisn@outlook.de>
2021-04-30 00:16:12 +0200

    added jwt authentication to startup

--- Emerald.Application/Startup.cs
+++ Emerald.Application/Startup.cs
@@ -1,6 +1,7 @@
 using AspNetCore.Identity.Mongo;
 using Emerald.Application.Services;
 using Emerald.Domain.Models.UserAggregate;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,10 +11,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Emerald.Application
@@ -43,13 +46,26 @@ namespace Emerald.Application
             });
 
             services.AddIdentityMongoDbProvider<User>(options =>
-                options.ConnectionString = Configuration["Mongo:ConnectionString"])
-                .AddDefaultTokenProviders();
+                options.ConnectionString = Configuration["Mongo:ConnectionString"]);
 
-            /*services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults
-            });*/
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
