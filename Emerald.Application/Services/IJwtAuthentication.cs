0effa0f1a95c06f7ec405b418cea051e6bc14c35
Philipp Hinz <phisn@outlook.de>
2021-04-29 20:12:13 +0200

    added jwt authentication service

---
+++ Emerald.Application/Services/IJwtAuthentication.cs
@@ -0,0 +1,13 @@
+﻿using Emerald.Domain.Models.UserAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Services
+{
+    public interface IJwtAuthentication
+    {
+        Task<string> GenerateToken(User user);
+    }
+}
---
+++ Emerald.Application/Services/JwtAuthentication.cs
@@ -0,0 +1,51 @@
+﻿using Emerald.Domain.Models.UserAggregate;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Services
+{
+    public class JwtAuthentication : IJwtAuthentication
+    {
+        private IConfiguration configuration;
+        private UserManager<User> userManager;
+
+        public JwtAuthentication(IConfiguration configuration, UserManager<User> userManager)
+        {
+            this.configuration = configuration;
+            this.userManager = userManager;
+        }
+
+        public async Task<string> GenerateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var roles = await userManager.GetRolesAsync(user);
+            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                configuration["Jwt:Issuer"],
+                configuration["Jwt:Issuer"],
+                claims,
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
