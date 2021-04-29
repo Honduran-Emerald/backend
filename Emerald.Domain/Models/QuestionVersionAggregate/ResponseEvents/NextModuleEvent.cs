459e4b5e2a7098f3d02f3667bab712512b638e67
Philipp Hinz <phisn@outlook.de>
2021-04-30 00:46:18 +0200

    added additional models to domain

---
+++ Emerald.Domain/Models/QuestionVersionAggregate/ResponseEvents/NextModuleEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emerald.Domain.Models.QuestionVersionAggregate
+{
+    public class NextModuleEvent
+    {
+    }
+}
