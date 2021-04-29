06580ac151559a192d6042b9ee59e73a890ec5b9
Philipp Hinz <phisn@outlook.de>
2021-04-29 19:50:52 +0200

    updated domain layer

---
+++ Emerald.Domain/Models/UserAggregate/User.cs
@@ -0,0 +1,18 @@
+﻿using AspNetCore.Identity.Mongo.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emerald.Domain.Models.UserAggregate
+{
+    public class User : MongoUser
+    {
+        public User()
+        {
+        }
+
+        public User(string userName) : base(userName)
+        {
+        }
+    }
+}
