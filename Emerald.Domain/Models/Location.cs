642c78873d3d273c9296b848610d91522597bcf3
Philipp Hinz <phisn@outlook.de>
2021-05-01 21:08:06 +0200

    added location model

---
+++ Emerald.Domain/Models/Location.cs
@@ -0,0 +1,30 @@
+﻿using GeoCoordinatePortable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vitamin.Value.Domain.SeedWork;
+
+namespace Emerald.Domain.Models
+{
+    public class Location : ValueObject
+    {
+        public double Longitude { get; private set; }
+        public double Latitude { get; private set; }
+
+        public Location(double longitude, double latitude)
+        {
+            Longitude = longitude;
+            Latitude = latitude;
+        }
+
+        public double Distance(Location location)
+            => new GeoCoordinate(Latitude, Longitude).GetDistanceTo(
+               new GeoCoordinate(location.Latitude, location.Longitude));
+
+        public override bool Equals(object obj)
+            => obj is Location location
+            && location.Distance(this) < 5;
+    }
+}
