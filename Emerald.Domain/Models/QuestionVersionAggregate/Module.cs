454c7f7ac80013ac8cb59922a92a03a753e68ff2
Philipp Hinz <phisn@outlook.de>
2021-05-01 21:23:33 +0200

    added components reference to module

--- Emerald.Domain/Models/QuestionVersionAggregate/Module.cs
+++ Emerald.Domain/Models/QuestionVersionAggregate/Module.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Emerald.Domain.Models.ComponentAggregate;
+using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Vitamin.Value.Domain.SeedWork;
 
 namespace Emerald.Domain.Models.QuestionVersionAggregate
 {
-    public class Module
+    public class Module : Entity
     {
+        public override ObjectId Id { get; protected set; }
+
+        public List<ObjectId> ComponentIds { get; private set; }
     }
 }
