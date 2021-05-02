feae3448b517c2adec0b969bc408584e5725593e
Philipp Hinz <phisn@outlook.de>
2021-05-03 00:56:24 +0200

    added basic cqrs viewmodel idea infrastructure

---
+++ Emerald.Infrastructure/ViewModels/QuestStatisticModel.cs
@@ -0,0 +1,31 @@
+﻿using Emerald.Domain.Models.QuestAggregate;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emerald.Infrastructure.ViewModels
+{
+    public class QuestStatisticModel
+    {
+        public ObjectId QuestId { get; private set; }
+
+        public int Votes { get; private set; }
+        public int Finishs { get; private set; }
+        public int Plays { get; private set; }
+
+        public QuestStatisticModel(ObjectId questId, int votes, int finishs, int plays)
+        {
+            QuestId = questId;
+            Votes = votes;
+            Finishs = finishs;
+            Plays = plays;
+        }
+
+        private QuestStatisticModel()
+        {
+        }
+    }
+}
