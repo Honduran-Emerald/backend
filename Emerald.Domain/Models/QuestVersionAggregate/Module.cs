be7a04c843845b85b6a3ad4ca2db27c2e5d7cfce
Philipp Hinz <phisn@outlook.de>
2021-05-03 00:52:23 +0200

    added questversion reference and title to module

--- Emerald.Domain/Models/QuestVersionAggregate/Module.cs
+++ Emerald.Domain/Models/QuestVersionAggregate/Module.cs
@@ -8,16 +8,32 @@ using Vitamin.Value.Domain.SeedWork;
 
 namespace Emerald.Domain.Models.QuestVersionAggregate
 {
-    public class Module : Entity
+    [BsonDiscriminator(RootClass = true)]
+    [BsonKnownTypes()]
+    public abstract class Module : Entity
     {
         public override ObjectId Id { get; protected set; }
 
         public List<ObjectId> ComponentIds { get; private set; }
+        public string Title { get; private set; }
+        
+        [BsonIgnore]
+        public QuestVersion QuestVersion { get; set; }
 
-        public Module()
+        public Module(string title)
+            : this()
+        {
+            Title = title;
+        }
+
+        private Module()
         {
             ComponentIds = new List<ObjectId>();
         }
 
+        public void SetTitle(string title)
+        {
+
+        }
     }
 }
