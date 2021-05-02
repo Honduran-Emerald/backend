7d829875da58224f9ce8cdfa9c7270193484e316
Philipp Hinz <phisn@outlook.de>
2021-05-02 14:54:13 +0200

    added component controller with test image upload

---
+++ Emerald.Application/Controllers/ComponentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Controllers
+{
+    [Route("component")]
+    [ApiController]
+    public class ComponentController : ControllerBase
+    {
+
+        [HttpPost("addimage")]
+        public async Task<IActionResult> AddImageComponent(
+            IFormFile image)
+        {
+            return Ok(image.FileName);
+        }
+    }
+}
