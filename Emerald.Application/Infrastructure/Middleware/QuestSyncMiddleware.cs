dab87e81a4a8cd081fffe5cd427a4b4f673591d0
Philipp Hinz <phisn@outlook.de>
2021-05-16 18:39:26 +0200

    implemented questsyncmiddleware

--- Emerald.Application/Infrastructure/Middleware/QuestSyncMiddleware.cs
+++ Emerald.Application/Infrastructure/Middleware/QuestSyncMiddleware.cs
@@ -1,23 +1,53 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Emerald.Domain.Models.UserAggregate;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
-namespace Emerald.Application.Infrastructure.Middleware
+namespace Emerald.Application.Infrastructure.ActionFilter
 {
-    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
     public class QuestSyncMiddleware
     {
-        private readonly RequestDelegate _next;
+        private readonly RequestDelegate next;
 
         public QuestSyncMiddleware(RequestDelegate next)
         {
-            _next = next;
+            this.next = next;
         }
 
-        public Task Invoke(HttpContext httpContext)
+        public async Task Invoke(HttpContext context, UserManager<User> userManager)
         {
+            if (context.User.Identity.IsAuthenticated)
+            {
+                context.Response.OnStarting(async () =>
+                {
+                    User user = await userManager.GetUserAsync(context.User);
+                    context.Response.Headers.Add("Sync-Token", user.SyncToken);
+                });
 
-            return _next(httpContext);
+                User user = await userManager.GetUserAsync(context.User);
+                await userManager.UpdateAsync(user);
+
+                if (context.Request.Headers.TryGetValue("Sync-Token", out var syncToken) &&
+                    syncToken.Count > 0 &&
+                    user.SyncToken != syncToken[0])
+                {
+                    context.Response.StatusCode = 250;
+                    await context.Response.WriteAsJsonAsync(new
+                    {
+                        message = "Got invalid sync token"
+                    });
+
+                    return;
+                }
+
+            }
+
+            await next(context);
         }
     }
 
