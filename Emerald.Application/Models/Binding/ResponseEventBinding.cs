66f16dbb2022b99c121ebfd4057960ec66bcf0bb
Philipp Hinz <phisn@outlook.de>
2021-05-16 15:37:18 +0200

    added responsebinding for event requests

---
+++ Emerald.Application/Models/Binding/ResponseEventBinding.cs
@@ -0,0 +1,13 @@
+﻿using Emerald.Application.Models.Quest.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Models.Binding
+{
+    public class ResponseEventBinding
+    {
+        public ResponseEventModel ResponseEvent { get; set; }
+    }
+}
