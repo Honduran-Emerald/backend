12580a57d3550e6cfb3240dab58ce69d5e939641
Philipp Hinz <phisn@outlook.de>
2021-05-16 15:34:58 +0200

    added module application models

---
+++ Emerald.Application/Models/Quest/Module/ChoiceModuleModel.cs
@@ -0,0 +1,19 @@
+﻿using Emerald.Application.Models.Quest.Component;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Models.Quest.Module
+{
+    public class ChoiceModuleModel : ModuleModel
+    {
+        public List<string> Choices { get; set; }
+
+        public ChoiceModuleModel(List<string> choices, List<ComponentModel> components) 
+            : base(ModuleType.Choice, components)
+        {
+            Choices = choices;
+        }
+    }
+}
---
+++ Emerald.Application/Models/Quest/Module/ModuleModel.cs
@@ -0,0 +1,20 @@
+﻿using Emerald.Application.Models.Quest.Component;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Models.Quest.Module
+{
+    public class ModuleModel
+    {
+        public ModuleType Type { get; set; }
+        public List<ComponentModel> Components { get; set; }
+
+        public ModuleModel(ModuleType type, List<ComponentModel> components)
+        {
+            Type = type;
+            Components = components;
+        }
+    }
+}
