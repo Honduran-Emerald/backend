5665fb3b6eabeaa42886e6450be99e4d5126efa2
Philipp Hinz <phisn@outlook.de>
2021-05-16 15:46:35 +0200

    added tracker and quests to user domain model

--- Emerald.Domain/Models/UserAggregate/User.cs
+++ Emerald.Domain/Models/UserAggregate/User.cs
@@ -1,18 +1,39 @@
 ﻿using AspNetCore.Identity.Mongo.Model;
+using Emerald.Domain.Models.TrackerAggregate;
+using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Vitamin.Value.Domain.SeedWork;
 
 namespace Emerald.Domain.Models.UserAggregate
 {
     public class User : MongoUser
     {
-        public User()
+        public List<ObjectId> QuestIds { get; private set; }
+        public List<ObjectId> TrackerIds { get; private set; }
+        public ObjectId? ActiveTrackerId { get; private set; }
+
+        public User() : base()
         {
+            QuestIds = new List<ObjectId>();
+            TrackerIds = new List<ObjectId>();
         }
 
         public User(string userName) : base(userName)
         {
+            QuestIds = new List<ObjectId>();
+            TrackerIds = new List<ObjectId>();
+        }
+
+        public void SetActiveTracker(Tracker tracker)
+        {
+            if (TrackerIds.Contains(tracker.Id) == false)
+            {
+                throw new DomainException("Can not set missing tracker as active");
+            }
+
+            ActiveTrackerId = tracker.Id;
         }
     }
 }
