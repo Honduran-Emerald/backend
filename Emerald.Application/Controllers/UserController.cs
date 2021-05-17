0bbb6a37021257c0e2dd5f27fe95498a2284a077
Philipp Hinz <phisn@outlook.de>
2021-05-17 22:48:09 +0200

    added user controller

---
+++ Emerald.Application/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Emerald.Application.Models.Response;
+using Emerald.Application.Services.Factories;
+using Emerald.Domain.Models.UserAggregate;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Controllers
+{
+    [ApiController]
+    [Route("user")]
+    public class UserController : ControllerBase
+    {
+        private UserModelFactory userModelFactory;
+        private UserManager<User> userManager;
+
+        public UserController(UserModelFactory userModelFactory, UserManager<User> userManager)
+        {
+            this.userModelFactory = userModelFactory;
+            this.userManager = userManager;
+        }
+
+        [Authorize]
+        [HttpGet("me")]
+        public async Task<ActionResult<UserMeResponse>> Me()
+        {
+            return Ok(new UserMeResponse
+            {
+                User = await userModelFactory.Create(
+                    await userManager.GetUserAsync(User))
+            });
+        }
+    }
+}
