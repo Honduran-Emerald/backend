24ea2e19dd3f53dbad2685bfd0798852a9ddffee
Philipp Hinz <phisn@outlook.de>
2021-05-17 22:46:31 +0200

    added location model

---
+++ Emerald.Application/Models/LocationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Models
+{
+    public class LocationModel
+    {
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
