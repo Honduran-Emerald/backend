34e72b4a078b2fbdf37d63eecb310f34dc7b3f84
Philipp Hinz <phisn@outlook.de>
2021-05-17 19:36:47 +0200

    added questmodelfactory

---
+++ Emerald.Application/Services/Factories/IQuestModelFactory.cs
@@ -0,0 +1,14 @@
+﻿using Emerald.Application.Models.Quest;
+using Emerald.Domain.Models.QuestAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Services.Factories
+{
+    public interface IQuestModelFactory
+    {
+        Task<QuestModel> Create(Quest quest);
+    }
+}
---
+++ Emerald.Application/Services/Factories/QuestModelFactory.cs
@@ -0,0 +1,45 @@
+﻿using Emerald.Application.Models.Quest;
+using Emerald.Domain.Models.QuestAggregate;
+using Emerald.Domain.Models.QuestVersionAggregate;
+using Emerald.Infrastructure.ViewModelHandlers;
+using Emerald.Infrastructure.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Services.Factories
+{
+    public class QuestModelFactory : IQuestModelFactory
+    {
+        private QuestViewModelStash questStash;
+
+        public QuestModelFactory(QuestViewModelStash questStash)
+        {
+            this.questStash = questStash;
+        }
+
+        public async Task<QuestModel> Create(Quest quest)
+        {
+            QuestVersion version = quest.GetStableQuestVersion();
+            QuestViewModel viewModel = await questStash.Get(quest.Id);
+
+            return new QuestModel
+            {
+                Id = quest.Id,
+                Owner = quest.OwnerUserId,
+
+                Title = version.Title,
+                Description = version.Description,
+                Image = version.Image,
+                Version = version.Version,
+
+                CreatedAt = version.CreatedAt,
+                
+                Votes = viewModel.Votes,
+                Plays = viewModel.Plays,
+                Finishs = viewModel.Finishs
+            };
+        }
+    }
+}
