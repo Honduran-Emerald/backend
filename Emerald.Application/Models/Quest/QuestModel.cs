fee2608af3448964976e2bae26e3ac6db13b4940
Philipp Hinz <phisn@outlook.de>
2021-05-17 19:36:06 +0200

    added questmodel

---
+++ Emerald.Application/Models/Quest/QuestModel.cs
@@ -0,0 +1,27 @@
+﻿using Emerald.Domain.Models.QuestAggregate;
+using Emerald.Domain.Models.QuestVersionAggregate;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Models.Quest
+{
+    public class QuestModel
+    {
+        public ObjectId Id { get; set; }
+        public ObjectId Owner { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public long Version { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public int Votes { get; set; }
+        public int Plays { get; set; }
+        public int Finishs { get; set; }
+    }
+}
