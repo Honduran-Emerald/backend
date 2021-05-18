63e20463df4569bad5a90c0fe2d9d128fca65dc5
Philipp Hinz <phisn@outlook.de>
2021-05-18 15:56:55 +0200

    added common responsedomainevent handlers

---
+++ Emerald.Domain/EventHandler/ExperienceResponseDomainEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using Emerald.Domain.Events;
+using Emerald.Domain.Models.ModuleAggregate.ResponseEvents;
+using Emerald.Domain.Models.UserAggregate;
+using Emerald.Infrastructure.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Emerald.Domain.EventHandler
+{
+    public class ExperienceResponseDomainEventHandler 
+        : INotificationHandler<QuestResponseDomainEvent<ExperienceResponseEvent>>
+    {
+        private IUserRepository userRepository;
+
+        public ExperienceResponseDomainEventHandler(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public async Task Handle(QuestResponseDomainEvent<ExperienceResponseEvent> notification, CancellationToken cancellationToken)
+        {
+            User user = await userRepository.Get(notification.UserId);
+            user.AddExperience(notification.Event.Experience);
+            await userRepository.Update(user);
+        }
+    }
+}
---
+++ Emerald.Domain/EventHandler/ModuleFinishResponseDomainEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using Emerald.Domain.Events;
+using Emerald.Domain.Models.ModuleAggregate;
+using Emerald.Domain.Models.TrackerAggregate;
+using Emerald.Domain.Repositories;
+using Emerald.Infrastructure.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Emerald.Domain.EventHandler
+{
+    class ModuleFinishResponseDomainEventHandler
+        : INotificationHandler<QuestResponseDomainEvent<ModuleFinishResponseEvent>>
+    {
+        private ITrackerRepository trackerRepository;
+
+        public ModuleFinishResponseDomainEventHandler(ITrackerRepository trackerRepository)
+        {
+            this.trackerRepository = trackerRepository;
+        }
+
+        public async Task Handle(QuestResponseDomainEvent<ModuleFinishResponseEvent> notification, CancellationToken cancellationToken)
+        {
+            Tracker tracker = await trackerRepository.Get(notification.TrackerId);
+            tracker.AddTrackerPath(new TrackerPath(notification.Event.ModuleId));
+            await trackerRepository.Update(tracker);
+        }
+    }
+}
