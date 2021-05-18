b067ef3dfc289b0f813b1f9601fe6075a9e8beef
Philipp Hinz <phisn@outlook.de>
2021-05-18 15:58:41 +0200

    added objective to modulemodel

--- Emerald.Application/Models/Quest/Module/ModuleModel.cs
+++ Emerald.Application/Models/Quest/Module/ModuleModel.cs
@@ -6,13 +6,16 @@ namespace Emerald.Application.Models.Quest.Module
     public class ModuleModel
     {
         public string ModuleId { get; set; }
+
+        public string Objective { get; set; }
         public ModuleType Type { get; set; }
         public List<ComponentModel> Components { get; set; }
 
-        public ModuleModel(string moduleId, ModuleType type, List<ComponentModel> components)
+        public ModuleModel(string moduleId, ModuleType type, string objective, List<ComponentModel> components)
         {
             ModuleId = moduleId;
             Type = type;
+            Objective = objective;
             Components = components;
         }
     }
