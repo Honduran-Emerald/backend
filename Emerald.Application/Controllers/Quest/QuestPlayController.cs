2dcab9d02174971a53ca034ca55ca57b962b0e7e
Philipp Hinz <phisn@outlook.de>
2021-05-22 16:00:53 +0200

    implemented query and publish in questplaycontroller

--- Emerald.Application/Controllers/Quest/QuestPlayController.cs
+++ Emerald.Application/Controllers/Quest/QuestPlayController.cs
@@ -11,6 +11,12 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Vitamin.Value.Domain.SeedWork;
 using Emerald.Application.Models.Response.Quest;
+using Emerald.Application.Models.Request;
+using Emerald.Infrastructure.Repositories;
+using MongoDB.Driver.Linq;
+using Emerald.Domain.Models.TrackerAggregate;
+using Microsoft.EntityFrameworkCore;
+using Emerald.Application.Services.Factories;
 
 namespace Emerald.Application.Controllers
 {
@@ -19,15 +25,45 @@ namespace Emerald.Application.Controllers
     [Route("/play")]
     public class QuestPlayController : ControllerBase
     {
-        private ResponseEventModelFactory responseEventFactory;
-        private QuestPlayService questPlayService;
-        private UserManager<User> userManager;
+        private ITrackerRepository trackerRepository;
+        private IUserRepository userRepository;
 
-        public QuestPlayController(ResponseEventModelFactory responseEventFactory, QuestPlayService questPlayService, UserManager<User> userManager)
+        private ResponseEventModelFactory responseEventFactory;
+        private TrackerModelFactory trackerFactory;
+        private QuestPlayService questPlayService;
+
+        public QuestPlayController(ITrackerRepository trackerRepository, IUserRepository userRepository, ResponseEventModelFactory responseEventFactory, TrackerModelFactory trackerFactory, QuestPlayService questPlayService)
         {
+            this.trackerRepository = trackerRepository;
+            this.userRepository = userRepository;
             this.responseEventFactory = responseEventFactory;
+            this.trackerFactory = trackerFactory;
             this.questPlayService = questPlayService;
-            this.userManager = userManager;
+        }
+
+        [HttpGet("query")]
+        public async Task<ActionResult<QuestPlayQueryResponse>> Query(
+            [FromForm] QuestPlayQueryRequest request)
+        {
+            User user = await  userRepository.Get(User);
+
+            IMongoQueryable<Tracker> queryable = trackerRepository.GetQueryable()
+                .Where(t => t.UserId == user.Id);
+
+            if (request.Unfinished)
+            {
+                queryable = queryable.Where(t => t.Finished == false);
+            }
+
+            return Ok(new QuestPlayQueryResponse(
+                trackers: await trackerFactory.Create(await queryable.ToListAsync())
+            ));
+        }
+
+        [HttpGet("querytrackernodes")]
+        public async Task<IActionResult> QueryTrackerNodes()
+        {
+            return Ok();
         }
 
         [HttpPost("event/position")]
@@ -45,7 +81,7 @@ namespace Emerald.Application.Controllers
             try
             {
                 ResponseEventCollection responseEvent = await questPlayService.HandleEvent(
-                    await userManager.GetUserAsync(User),
+                    await userRepository.Get(User),
                     new ChoiceRequestEvent(choiceEvent.Choice));
 
                 return Ok(new QuestPlayEventResponse
