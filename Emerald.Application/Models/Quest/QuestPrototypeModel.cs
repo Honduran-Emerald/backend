816319611ee5a540ec1593ca0379bc874c9afbde
Philipp Hinz <phisn@outlook.de>
2021-05-25 22:41:07 +0200

    fix

--- Emerald.Application/Models/Quest/QuestPrototypeModel.cs
+++ Emerald.Application/Models/Quest/QuestPrototypeModel.cs
@@ -10,5 +10,10 @@ namespace Emerald.Application.Models.Quest
         public string Title { get; set; }
         public string ImageId { get; set; }
 
+        public QuestPrototypeModel(string title, string imageId)
+        {
+            Title = title;
+            ImageId = imageId;
+        }
     }
 }
