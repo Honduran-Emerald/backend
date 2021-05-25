05bb18ca6cc405e972700cc36283cafbabbaab29
Philipp Hinz <phisn@outlook.de>
2021-05-25 22:40:41 +0200

    added request

---
+++ Emerald.Application/Models/Quest/QuestPrototypeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Models.Quest
+{
+    public class QuestPrototypeModel
+    {
+        public string Title { get; set; }
+        public string ImageId { get; set; }
+
+    }
+}
---
+++ Emerald.Application/Models/Response/Quest/QuestCreateGetResponse.cs
@@ -0,0 +1,21 @@
+﻿using Emerald.Application.Models.Quest;
+using Emerald.Application.Models.Quest.Module;
+using Emerald.Domain.Models.PrototypeAggregate;
+using Emerald.Domain.Models.QuestAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Models.Response.Quest
+{
+    public class QuestCreateGetResponse
+    {
+        QuestPrototype QuestPrototype { get; set; }
+
+        public QuestCreateGetResponse(QuestPrototype questPrototype)
+        {
+            QuestPrototype = questPrototype;
+        }
+    }
+}
