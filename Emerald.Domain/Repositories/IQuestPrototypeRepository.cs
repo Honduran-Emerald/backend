38c9edf67ee102c06bd2e1386d8f001c03b0b673
Philipp Hinz <phisn@outlook.de>
2021-05-25 13:33:44 +0200

    added questprototype repository interface

---
+++ Emerald.Domain/Repositories/IQuestPrototypeRepository.cs
@@ -0,0 +1,17 @@
+﻿using Emerald.Domain.Models.PrototypeAggregate;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emerald.Domain.Repositories
+{
+    public interface IQuestPrototypeRepository
+    {
+        Task<QuestPrototype> Get(ObjectId id);
+        Task Update(QuestPrototype questPrototype);
+        Task Add(QuestPrototype questPrototype);
+    }
+}
