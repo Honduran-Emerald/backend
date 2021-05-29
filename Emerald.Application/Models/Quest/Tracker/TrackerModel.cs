7421b9d1de661de889c8be920094de9d7da744db
Philipp Hinz <phisn@outlook.de>
2021-05-30 01:21:09 +0200

    @Felex

--- Emerald.Application/Models/Quest/Tracker/TrackerModel.cs
+++ Emerald.Application/Models/Quest/Tracker/TrackerModel.cs
@@ -10,18 +10,26 @@ namespace Emerald.Application.Models.Quest.Tracker
     public class TrackerModel
     {
         public ObjectId TrackerId { get; set; }
+
         public bool NewestQuestVersion { get; set; }
         public bool Finished { get; set; }
         public VoteType Vote { get; set; }
         public DateTime CreationTime { get; set; }
 
-        public TrackerModel(ObjectId trackerId, bool newestQuestVersion, bool finished, VoteType vote, DateTime creationTime)
+        public string QuestName { get; set; }
+        public string Objective { get; set; }
+        public string Author { get; set; }
+
+        public TrackerModel(ObjectId trackerId, bool newestQuestVersion, bool finished, VoteType vote, DateTime creationTime, string questName, string objective, string author)
         {
             TrackerId = trackerId;
             NewestQuestVersion = newestQuestVersion;
             Finished = finished;
             Vote = vote;
             CreationTime = creationTime;
+            QuestName = questName;
+            Objective = objective;
+            Author = author;
         }
 
         public TrackerModel()
@@ -30,6 +38,9 @@ namespace Emerald.Application.Models.Quest.Tracker
             Finished = default!;
             Vote = default!;
             CreationTime = default!;
+            QuestName = default!;
+            Objective = default!;
+            Author = default!;
         }
     }
 }
