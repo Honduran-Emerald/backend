15b16be36464f9fb4165edf9114a39723b0b54ec
Philipp Hinz <phisn@outlook.de>
2021-06-09 21:13:29 +0200

    added questprototype and factory for it

---
+++ Emerald.Application/Models/Prototype/QuestPrototypeModel.cs
@@ -0,0 +1,48 @@
+﻿using Emerald.Application.Models.Quest;
+using Emerald.Application.Models.Quest.Tracker;
+using Emerald.Domain.Models;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Models.Prototype
+{
+    public class QuestPrototypeModel
+    {
+        public ObjectId QuestId { get; set; }
+
+        public QuestModel? Quest { get; set; }
+
+        public string? Title { get; set; }
+        public string? ImageId { get; set; }
+        public string? Description { get; set; }
+
+        public Location? Location { get; set; }
+        public string? LocationName { get; set; }
+
+        public string? AgentProfileName { get; set; }
+        public string? AgentProfileImageId { get; set; }
+
+        public bool Released { get; set; }
+        public bool Public { get; set; }
+        public bool Outdated { get; set; }
+
+        public QuestPrototypeModel(ObjectId questId, QuestModel? quest, string? title, string? imageId, string? description, Location? location, string? locationName, string? agentProfileName, string? agentProfileImageId, bool released, bool @public, bool outdated)
+        {
+            QuestId = questId;
+            Quest = quest;
+            Title = title;
+            ImageId = imageId;
+            Description = description;
+            Location = location;
+            LocationName = locationName;
+            AgentProfileName = agentProfileName;
+            AgentProfileImageId = agentProfileImageId;
+            Released = released;
+            Public = @public;
+            Outdated = outdated;
+        }
+    }
+}
