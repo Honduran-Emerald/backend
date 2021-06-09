c8019510a4ff7003c199d910551904ebb1b222d8
Philipp Hinz <phisn@outlook.de>
2021-06-09 21:10:21 +0200

    added userservice to get current user

---
+++ Emerald.Application/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using Emerald.Domain.Models.UserAggregate;
+using Emerald.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private IHttpContextAccessor httpContextAccessor;
+        private IUserRepository userRepository;
+
+        public UserService(IHttpContextAccessor httpContextAccessor, IUserRepository userRepository)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+            this.userRepository = userRepository;
+        }
+
+        public async Task<User> CurrentUser()
+        {
+            if (httpContextAccessor.HttpContext == null)
+            {
+                throw new Exception("Unable to get current user if HttpContext is null");
+            }
+
+            return await userRepository.Get(httpContextAccessor.HttpContext.User);
+        }
+    }
+}
