2fcfee3d5c6a6dae8ec4cefb32f173dc86c2b748
Philipp Hinz <phisn@outlook.de>
2021-06-09 21:15:19 +0200

    added user get request

--- Emerald.Application/Controllers/UserController.cs
+++ Emerald.Application/Controllers/UserController.cs
@@ -1,9 +1,12 @@
 ﻿using Emerald.Application.Models.Response;
+using Emerald.Application.Services;
 using Emerald.Application.Services.Factories;
 using Emerald.Domain.Models.UserAggregate;
+using Emerald.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,12 +20,18 @@ namespace Emerald.Application.Controllers
     {
         private UserModelFactory userModelFactory;
         private UserManager<User> userManager;
+        private IUserRepository userRepository;
+        private IUserService userService;
 
-        public UserController(UserModelFactory userModelFactory, UserManager<User> userManager)
+        public UserController(UserModelFactory userModelFactory, UserManager<User> userManager, IUserRepository userRepository, IUserService userService)
         {
             this.userModelFactory = userModelFactory;
             this.userManager = userManager;
+            this.userRepository = userRepository;
+            this.userService = userService;
         }
+
+
 
         /// <summary>
         /// Get profile information about current authorized user
@@ -34,7 +43,20 @@ namespace Emerald.Application.Controllers
         {
             return Ok(new UserMeResponse(
                 await userModelFactory.Create(
-                    await userManager.GetUserAsync(User))));
+                    await userService.CurrentUser())));
+        }
+
+        /// <summary>
+        /// Get profile information about another user
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        [HttpGet("get")]
+        public async Task<ActionResult<UserMeResponse>> Get(
+            [FromQuery] ObjectId userId)
+        {
+            return Ok(new UserMeResponse(
+                await userModelFactory.Create(await userRepository.Get(userId))));
         }
     }
 }
