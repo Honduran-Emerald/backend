63f8fdeb5bca7e1dbbe7afb9451fd14f582d0239
Philipp Hinz <phisn@outlook.de>
2021-06-10 15:06:48 +0200

    added imageservice

---
+++ Emerald.Application/Services/IImageService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Services
+{
+    public interface IImageService
+    {
+        string ImageToUrl(string imageId);
+        Task<Stream> Download(string imageId);
+        Task<string> Upload(Stream stream);
+    }
+}
---
+++ Emerald.Application/Services/ImageService.cs
@@ -0,0 +1,47 @@
+﻿using Emerald.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using MongoDB.Driver.GridFS;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Vitamin.Value.Domain;
+
+namespace Emerald.Application.Services
+{
+    public class ImageService : IImageService
+    {
+        private IHttpContextAccessor httpContextAccessor;
+        private IGridFSBucket imageBucket;
+
+        public ImageService(IHttpContextAccessor httpContextAccessor, IMongoDbContext dbContext)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+
+            imageBucket = new GridFSBucket(dbContext.Emerald, new GridFSBucketOptions
+            {
+                BucketName = "Images"
+            });
+
+        }
+
+        public string ImageToUrl(string imageId)
+        { 
+            var request = httpContextAccessor.HttpContext!.Request;
+            return $"{request.Scheme}://{request.Host}image/{imageId}";
+        }
+
+        public async Task<Stream> Download(string imageId)
+        {
+            return await imageBucket.OpenDownloadStreamByNameAsync(imageId);
+        }
+
+        public async Task<string> Upload(Stream stream)
+        {
+            string imageId = Utility.RandomString(16);
+            await imageBucket.UploadFromStreamAsync(imageId, stream);
+            return imageId;
+        }
+    }
+}
