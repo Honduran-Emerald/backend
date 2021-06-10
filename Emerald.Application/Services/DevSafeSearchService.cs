e0f4c1847cd93e009f28127eaaa44b6c3ef48768
Philipp Hinz <phisn@outlook.de>
2021-06-10 15:06:36 +0200

    added safesearchservice

---
+++ Emerald.Application/Services/DevSafeSearchService.cs
@@ -0,0 +1,21 @@
+﻿using Google.Cloud.Vision.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Services
+{
+    public class DevSafeSearchService : ISafeSearchService
+    {
+        public Task<bool> Detect(string imageId)
+        {
+            return Task.FromResult(false);
+        }
+
+        public Task<bool> Detect(Image image)
+        {
+            return Task.FromResult(false);
+        }
+    }
+}
