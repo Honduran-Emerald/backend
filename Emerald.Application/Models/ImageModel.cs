f22ed5bd7104ed22aa84edd88db8b16028b9799e
Philipp Hinz <phisn@outlook.de>
2021-06-13 17:48:25 +0200

    added imagemodel

---
+++ Emerald.Application/Models/ImageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emerald.Application.Models
+{
+    public class ImageModel
+    {
+        public int Reference { get; set; }
+        public string Image { get; set; }
+
+        public ImageModel()
+        {
+            Reference = default!;
+            Image = default!;
+        }
+    }
+}
