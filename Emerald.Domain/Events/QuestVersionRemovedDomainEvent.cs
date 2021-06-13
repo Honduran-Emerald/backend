aa2f8a43757ba9d4df6403435f9cc13cd749ac42
Philipp Hinz <phisn@outlook.de>
2021-06-13 17:41:53 +0200

    added questversionadded / removed event and handler

---
+++ Emerald.Domain/Events/QuestVersionRemovedDomainEvent.cs
@@ -0,0 +1,20 @@
+﻿using Emerald.Domain.Models.QuestVersionAggregate;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emerald.Domain.Events
+{
+    public class QuestVersionRemovedDomainEvent : INotification
+    {
+        public QuestVersion QuestVersion { get; set; }
+
+        public QuestVersionRemovedDomainEvent(QuestVersion questVersion)
+        {
+            QuestVersion = questVersion;
+        }
+    }
+}
---
+++ Emerald.Infrastructure/DomainEventHandler/QuestVersionRemovedDomainEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using Emerald.Domain.Events;
+using Emerald.Infrastructure.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Emerald.Infrastructure.DomainEventHandler
+{
+    public class QuestVersionRemovedDomainEventHandler : INotificationHandler<QuestVersionRemovedDomainEvent>
+    {
+        private IImageIndexService imageIndexService;
+
+        public QuestVersionRemovedDomainEventHandler(IImageIndexService imageIndexService)
+        {
+            this.imageIndexService = imageIndexService;
+        }
+
+        public async Task Handle(QuestVersionRemovedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            await imageIndexService.DecreaseImageReference(notification.QuestVersion.ImageId);
+        }
+    }
+}
