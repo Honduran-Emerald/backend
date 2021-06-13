7c6e9930c4d93bb13713c28b87c982317cc40c2d
Philipp Hinz <phisn@outlook.de>
2021-06-13 17:40:48 +0200

    added imageindex model and service for image referenece tracking

---
+++ Emerald.Infrastructure/Models/ImageIndexModel.cs
@@ -0,0 +1,38 @@
+﻿using Emerald.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vitamin.Value.Domain;
+using Vitamin.Value.Domain.SeedWork;
+
+namespace Emerald.Infrastructure.Models
+{
+    public class ImageIndexModel
+    {
+        public string Id { get; set; }
+        public int ReferenceCount { get; set; }
+
+        public ImageIndexModel(string id)
+        {
+            Id = id;
+            ReferenceCount = 0;
+        }
+
+        public ImageIndexModel()
+        {
+            Id = default!;
+        }
+
+        public void IncreaseReferenceCount()
+        {
+            ++ReferenceCount;
+        }
+
+        public void DecreaseReferenceCount()
+        {
+            --ReferenceCount;
+        }
+    }
+}
